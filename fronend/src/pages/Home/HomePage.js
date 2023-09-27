0584110b2bf8e0f96f41cbf14f98fdb48ed501bc
Phan Khai Duy <phankhaiduy0401@gmail.com>
2023-09-27 15:26:33 +0700

    Update Part 5 - Search

--- fronend/src/pages/Home/HomePage.js
+++ fronend/src/pages/Home/HomePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useReducer } from 'react';
 import Thumbnails from '../../components/Thumbnails/Thumbnails';
-import { getAll } from '../../services/foodService';
+import { getAll, search } from '../../services/foodService';
+import { useParams } from 'react-router-dom';
+import Search from '../../components/Search/Search';
 
 
 const initialState = { foods: [] };
@@ -17,13 +19,17 @@ const reducer = (state, action) => {
 export default function HomePage() {
     const [state, dispatch] = useReducer(reducer, initialState);
     const { foods } = state;
+    const { searchTerm } = useParams();
 
     useEffect(() => {
-        getAll().then(foods => dispatch({ type: 'FOODS_LOADED', payload: foods }));
-    }, []);
+        const loadFoods = searchTerm ? search(searchTerm) : getAll();
+
+        loadFoods.then(foods => dispatch({ type: 'FOODS_LOADED', payload: foods }));
+    }, [searchTerm]);
 
     return (
         <>
+            <Search />
             <Thumbnails foods={foods} />
         </>
     );
